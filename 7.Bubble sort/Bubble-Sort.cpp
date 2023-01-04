59b3ca92d477ccefef28974aa2ede2db9537056d
Ashab-Asir <ashabasir101@gmail.com>
2023-01-04 20:07:45 +0600

    bubble sort optimised

--- 7.Bubble sort/Bubble-Sort.cpp
+++ 7.Bubble sort/Bubble-Sort.cpp
@@ -2,11 +2,16 @@
 using namespace std;
 void bubble_sort(int arr[],int n){
   for(int i=0;i<n-1;i++){
+    int flag=0;
     for(int j=0;j<n-1-i;j++)
     {
         if(arr[j]>arr[j+1]){
             swap(arr[j],arr[j+1]);
+            flag=1;
         }
+    }
+    if(flag==0){
+        break;
     }
   }
 
