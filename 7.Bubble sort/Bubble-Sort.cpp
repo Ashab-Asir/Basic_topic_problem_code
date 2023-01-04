09aa96da1be151e0a54b4a78295196b9f5a78dd9
Ashab-Asir <ashabasir101@gmail.com>
2023-01-04 20:10:14 +0600

    swap function addded int bubble sort

--- 7.Bubble sort/Bubble-Sort.cpp
+++ 7.Bubble sort/Bubble-Sort.cpp
@@ -6,7 +6,9 @@ void bubble_sort(int arr[],int n){
     for(int j=0;j<n-1-i;j++)
     {
         if(arr[j]>arr[j+1]){
-            swap(arr[j],arr[j+1]);
+            int temp=arr[j];
+            arr[j]=arr[j+1];
+            arr[j+1]=temp;
             flag=1;
         }
     }
